fc4bf2aadea3d701e112b4ec728243a91a738fb8
Sulayman Colley <sulayman@writemedia.com>
2025-12-22 02:30:57 +0000

    Initial commit

---
+++ src/test/java/com/example/Simple_Ecommerce/SimpleEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Simple_Ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
